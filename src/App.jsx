0d48be056d2e10da19ac8651f0b34352d3d70139
tejagithubreddy <gorresrinivasareddy@gmail.com>
2025-07-20 13:06:01 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S109">
+    <Router basename="/CICD">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
